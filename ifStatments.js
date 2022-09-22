027df4731999c948fbfa5f6aa383e2d091246d37
Johnathon Ammon <johnathonammon@Johnathons-MacBook-Pro.local>
2022-09-21 21:19:45 -0600

    Initail commit

---
+++ ifStatments.js
@@ -0,0 +1,46 @@
+
+weather = "icy"
+
+if (weather === "sunny") {
+  console.log("sunscreen");
+} else if (weather === "rainy") {
+  console.log("umbrella");
+} else if (weather === "snowy") {
+  console.log("hat");
+} else if (weather === "icy") {
+  console.log("yak traks");
+} else {
+  console.log("good to go!");
+}
+
+
+numQuarters = 2
+
+if (numQuarters === 2){
+  console.log("I have enough money for a gumball");
+}
+  else if (numQuarters !== 2 ) {
+  console.log("I don't have enough money for a gumball");
+}
+
+
+
+
+
+var cupsOfFlour = 1;
+var hasSauce = true;
+
+if (cupsOfFlour === 1 && hasSauce === true) {
+  console.log("I cannot make pizza");
+}
+
+  else if (cupsOfFlour === 5 && hasSauce === false) {
+  console.log("I cannot make pizza");
+}
+  else if (cupsOfFlour === 2 && hasSauce === true) {
+    console.log("I can make pizza");
+}
+
+  else if (cupsOfFlour === 3 && hasSauce === true) {
+  console.log("I can make pizza");
+}
